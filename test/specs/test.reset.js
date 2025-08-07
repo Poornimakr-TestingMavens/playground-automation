01d7ba9cbc5f66127dd943b04a3961dca4a8d2c1
Poornima K R <poornima.ramachandran@testingmavens.com>
2025-08-07 12:06:40 +0530

    HTML report for Automation

---
+++ test/specs/test.reset.js
@@ -0,0 +1,46 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+import homePage from '../pageobjects/homePage.js'
+import loginDetails from '../data/loginDetails.json'
+import cartPage from '../pageobjects/cartPage.js'
+import checkOutPage from '../pageobjects/checkOutPage.js'
+
+
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+        await LoginPage.login(loginDetails.email, loginDetails.password);
+        await LoginPage.wishlistButton.waitForDisplayed({timeout:5000});
+        await LoginPage.homePage();
+        await expect(LoginPage.profileName).toBeDisplayed({timeout:5000});
+
+    })
+    it('Homepage displays all categories to buy',async()=>{
+        await homePage.shopButton.click();
+
+        await expect(homePage.categoryButton).toBeDisplayed({timeout:5000});
+        await expect(homePage.brandButton).toBeDisplayed({timeout:5000});
+        await expect(homePage.shopByColor).toBeDisplayed();
+    })
+   
+    it('Add a product to cart',async()=>{
+        await cartPage.addRandomProductToCart();
+        await expect(cartPage.cartPageMessage).toBeDisplayed();
+    })
+    it('go to buy now page of cart',async()=>{
+        await checkOutPage.buyNowPage();
+        await expect(checkOutPage.cartPageValidation).toBeDisplayed();
+        await checkOutPage.priceValidationOfCart();
+        await expect(checkOutPage.resetCartButtonvalidation).toBeDisplayed();
+    })
+    it('working of reset cart button',async()=>{
+        await checkOutPage.resetCartButtonvalidation.click();
+        await expect(checkOutPage.resetSuccessMessage).toBeDisplayed();
+
+
+    })
+
+})
+
+
